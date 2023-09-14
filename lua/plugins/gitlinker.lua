ea304e39caa1f93df110bd173434fed216a44f4c
Nick deLannoy <nick.delannoy@gmail.com>
2023-09-14 11:39:34 -0500

    add open in github mapping

--- lua/plugins/gitlinker.lua
+++ lua/plugins/gitlinker.lua
@@ -9,6 +9,18 @@ return {
       }
       require("which-key").register(mapping, { mode = "n" })
       require("which-key").register(mapping, { mode = "v" })
+      vim.api.nvim_set_keymap(
+        "n",
+        "<leader>go",
+        '<cmd>lua require"gitlinker".get_buf_range_url("n", {action_callback = require"gitlinker.actions".open_in_browser})<cr>',
+        { desc = "Open in GitHub", silent = true }
+      )
+      vim.api.nvim_set_keymap(
+        "v",
+        "<leader>go",
+        '<cmd>lua require"gitlinker".get_buf_range_url("v", {action_callback = require"gitlinker.actions".open_in_browser})<cr>',
+        { desc = "Open in GitHub" }
+      )
     end,
   },
 }
