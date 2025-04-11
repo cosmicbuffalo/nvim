d8d1fa793401a677f50eb8b90b78d5384f69b34f
Nicholas deLannoy <ndelannoy@paypal.com>
2025-04-11 14:19:32 +0000

    turn off markdown

--- lua/plugins/ft.lua
+++ lua/plugins/ft.lua
@@ -29,6 +29,7 @@ return {
   },
   {
     'MeanderingProgrammer/markdown.nvim',
+    enabled = false,
     main = "render-markdown",
     opts = {},
     name = 'render-markdown', -- Only needed if you have another plugin named markdown.nvim
